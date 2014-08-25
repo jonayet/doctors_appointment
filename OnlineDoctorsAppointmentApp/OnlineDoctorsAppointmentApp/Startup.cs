5ec348b020daaa76af1309a3410ec3865eb0a5bc
kazirashedulhaque <rashedul.haque.se@gmail.com>
2014-08-25 18:55:15 +0600

    Online Doctor's Appointment project creation.

---
+++ OnlineDoctorsAppointmentApp/OnlineDoctorsAppointmentApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineDoctorsAppointmentApp.Startup))]
+namespace OnlineDoctorsAppointmentApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
