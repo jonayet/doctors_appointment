88aac54d0b96316b6fd0dac8f2374f554aa65448
RizwanRumi <rizwan.cse33@gmail.com>
2014-08-31 17:05:29 +0600

    SearchDoctor Controller and its functionality added

---
+++ OnlineDoctorsAppointmentApp/OnlineDoctorsAppointmentApp/Controllers/SearchDoctorController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineDoctorsAppointmentApp.Models;
+
+namespace OnlineDoctorsAppointmentApp.Controllers
+{
+    public class SearchDoctorController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: /SearchDoctor/
+        public ActionResult Index()
+        {
+            ViewBag.ChamberId = new SelectList(db.Chambers, "ChamberId", "Name");
+            ViewBag.ChamberZone = new SelectList(db.Chambers, "ChamberId", "Zone");
+            ViewBag.DoctorId = new SelectList(db.Doctors, "DoctorId", "DoctorName");
+            ViewBag.Specialization = new SelectList(db.Doctors, "DoctorId", "Specialization");
+            var doctors = db.VisitingSessions.Include(v => v.Chambers).Include(v => v.Doctors);
+            return View(doctors.ToList());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(int? DoctorId, string Specialization, int? ChamberId, string ChamberZone, string dateSearchTextbox, string searchTextbox)
+        {
+            ViewBag.ChamberId = new SelectList(db.Chambers, "ChamberId", "Name");
+            ViewBag.ChamberZone = new SelectList(db.Chambers, "ChamberId", "Zone");
+            ViewBag.DoctorId = new SelectList(db.Doctors, "DoctorId", "DoctorName");
+            ViewBag.Specialization = new SelectList(db.Doctors, "DoctorId", "Specialization");
+
+            List<VisitingSession> adoctorList = new List<VisitingSession>();
+            if (ModelState.IsValid)
+            {
+                if (DoctorId != null)
+                {
+                    adoctorList = db.VisitingSessions.Where(c => c.DoctorId == DoctorId).ToList();
+                }
+                else if (Specialization != null)
+                {
+                    adoctorList = db.VisitingSessions.Where(c => c.Doctors.Specialization.Contains(Specialization)).ToList();
+                }
+                else if (ChamberId != null)
+                {
+                    adoctorList = db.VisitingSessions.Where(c => c.ChamberId == ChamberId).ToList();
+                }
+                else if (ChamberZone != null)
+                {
+                    adoctorList = db.VisitingSessions.Where(c => c.Chambers.Zone.Contains(ChamberZone)).ToList();
+                }
+                else if (dateSearchTextbox != null)
+                {
+                    // adoctorList = db.VisitingSessions.Where(c => c.StartTime == ((DateTime)dateSearchTextbox)).ToList();
+                }
+                else if (searchTextbox != null)
+                {
+                    adoctorList = db.VisitingSessions.Where(c => c.Chambers.Name.Contains(searchTextbox)).ToList();
+                }
+            }
+            return View(adoctorList);
+        }
+
+    }
+}
--- OnlineDoctorsAppointmentApp/OnlineDoctorsAppointmentApp/Models/Chamber.cs
+++ OnlineDoctorsAppointmentApp/OnlineDoctorsAppointmentApp/Models/Chamber.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Web;
@@ -10,8 +11,11 @@ namespace OnlineDoctorsAppointmentApp.Models
     public class Chamber
     {
         public int ChamberId { get; set; }
+        [Display(Name = "Chamber's Name")]
         public string Name { get; set; }
+        [Display(Name = "Chamber's Zone")]
         public string Zone { get; set; }
+        [Display(Name = "Chamber's Address")]
         public string Address { get; set; }
         public List<VisitingSession> VisitingSessions { get; set; }
     }
