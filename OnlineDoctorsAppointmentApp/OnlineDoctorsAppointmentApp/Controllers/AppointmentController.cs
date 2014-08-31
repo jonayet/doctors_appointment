97462f99a644e378ebf4c0a1b0e347328ec59290
esmotara <esmo.ice@gmail.com>
2014-08-31 22:17:49 +0600

    AppointmentController created

---
+++ OnlineDoctorsAppointmentApp/OnlineDoctorsAppointmentApp/Controllers/AppointmentController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineDoctorsAppointmentApp.Models;
+
+namespace OnlineDoctorsAppointmentApp.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        AppDbContext db = new AppDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(int chamberId, int doctorId, string patientName, string patientPhone, string patientEmail, DateTime appointmenTime, bool isNotificationSubscribed)
+        {
+            Appointment appointment = new Appointment()
+            {
+                AppointmentTime = appointmenTime,
+                ChamberId = chamberId,
+                DoctorId = doctorId,
+                PatientName = patientName,
+                PatientEmail = patientEmail,
+                PatientPhone = patientPhone,
+                IsNotificationSubscribed = isNotificationSubscribed,
+                IsAppointmentComplete = false,
+                IsAppointmentConfirm = false
+            };
+
+            db.Appointments.Add(appointment);
+            return View();
+        }
+    }
+}
