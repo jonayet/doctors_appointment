02d263d66a5cf42959c0db05a241b59fb7e6da07
RizwanRumi <rizwan.cse33@gmail.com>
2014-09-02 09:34:34 +0600

    searchdoctor Model added

---
+++ OnlineDoctorsAppointmentApp/OnlineDoctorsAppointmentApp/Models/SearchDoctor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineDoctorsAppointmentApp.Models
+{
+    public class SearchDoctor
+    {
+        public int   SearchDoctorId { get; set; }
+        public int VisitingSessionId { get; set; }
+        public virtual VisitingSession VisitingSessions { get; set; }
+    }
+}
