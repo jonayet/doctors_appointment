7f9fc63d5379450e8b0c42620219a53eaf7833d2
RizwanRumi <rizwan.cse33@gmail.com>
2014-09-08 15:04:40 +0530

    Revert "appointment index view"
    This reverts commit 63ec3f7e39f9a29a409fff374cd01847b6f96c03.

--- OnlineDoctorsAppointmentApp/OnlineDoctorsAppointmentApp/Controllers/AppointmentController.cs
+++ OnlineDoctorsAppointmentApp/OnlineDoctorsAppointmentApp/Controllers/AppointmentController.cs
@@ -9,7 +9,6 @@ using OnlineDoctorsAppointmentApp.Models;
 
 namespace OnlineDoctorsAppointmentApp.Controllers
 {
-
     public class AppointmentController : Controller
     {
         
@@ -18,7 +17,11 @@ namespace OnlineDoctorsAppointmentApp.Controllers
         private Chamber aChamber;
         private VisitingSession aVisitingSession;
 
-       
+        public ActionResult Index()
+        {
+            //var appointments = db.Appointments.ToList();
+            return RedirectToAction("Index","SearchDoctor");
+        }
 
 
         public ActionResult Index(Appointment anAppointment)
