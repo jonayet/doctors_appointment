f5eb60ca233ee254af45eefae5f1631957bd92d2
jonayet <jonayet.bu@gmail.com>
2014-09-08 10:16:26 +0600

    Login module complete

--- OnlineDoctorsAppointmentApp/OnlineDoctorsAppointmentApp/Controllers/HomeController.cs
+++ OnlineDoctorsAppointmentApp/OnlineDoctorsAppointmentApp/Controllers/HomeController.cs
@@ -16,14 +16,12 @@ namespace OnlineDoctorsAppointmentApp.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
